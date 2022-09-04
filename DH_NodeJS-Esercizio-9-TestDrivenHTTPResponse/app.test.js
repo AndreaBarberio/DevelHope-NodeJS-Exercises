1b027030877e881014ef2d58c9bebbd275fe6633
Andrea Barberio <andrea.barberio97@gmail.com>
2022-09-04 18:26:04 +0200

    Update: done from exercise 2 to 8

--- DH_NodeJS-Esercizio-9-TestDrivenHTTPResponse/app.test.js
+++ DH_NodeJS-Esercizio-9-TestDrivenHTTPResponse/app.test.js
@@ -1,6 +1,6 @@
 const supertest = require("supertest");
 
-const createApp = require("./app.js");
+const createApp = require("./app");
 
 const app = createApp();
 
@@ -10,7 +10,19 @@ test("GET /", async () => {
   const response = await request
     .get("/")
     .expect(200)
-    .expect("Content-Type", "application/json");
+    .expect("Content-Type", "text/html");
 
-  expect(response.body).toEqual({ location: "Earth" });
+  expect(response.text).toEqual("<html><body><h1>Welcome to the World Wide Web!</h1></body></html>");
 });
+/*
+Our integration test in app.test.js expects a JSON response.
+
+Change the test to expect an HTML response header: Content-Type: text/html
+
+Change the test to expect this HTML in response.text:
+
+Welcome to the World Wide Web!
+Run the test with npm test — it should fail.
+Update the code in app.js to send the HTTP response the test expects.
+Run the test with npm test — it should pass.
+*/
